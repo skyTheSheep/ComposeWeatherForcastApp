f803391a12f34310194efeef66cc850b43acc23f
skyTheSheep <ekwarin.y@gmail.com>
2026-05-05 12:59:17 +0700

    test test

--- app/src/main/java/com/skyyeoh/composeweatherforcastapp/utils/Constants.kt
+++ app/src/main/java/com/skyyeoh/composeweatherforcastapp/utils/Constants.kt
@@ -2,5 +2,5 @@ package com.skyyeoh.composeweatherforcastapp.utils
 
 object Constants {
     const val   BASE_URL = "https://api.openweathermap.org/"
-    const val API_KEY = "" // go to: https://openweathermap.org/forecast16
+    const val API_KEY = " " // go to: https://openweathermap.org/forecast16
 }
