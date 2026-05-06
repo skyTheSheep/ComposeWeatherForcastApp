ceb3dcca735f14b1da09da1491968724fd8e19ca
skyTheSheep <ekwarin.y@gmail.com>
2026-05-06 15:50:55 +0700

    done the second part out of three parts

--- app/src/main/java/com/skyyeoh/composeweatherforcastapp/screens/main/MainScreen.kt
+++ app/src/main/java/com/skyyeoh/composeweatherforcastapp/screens/main/MainScreen.kt
@@ -1,16 +1,22 @@
 package com.skyyeoh.composeweatherforcastapp.screens.main
 
 import android.util.Log
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.DividerDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -20,18 +26,22 @@ import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.NavController
-import coil3.compose.AsyncImage
 import com.bawp.jetweatherforecast.model.Weather
+import com.bawp.jetweatherforecast.model.WeatherItem
 import com.skyyeoh.composeweatherforcastapp.data.DataOrException
 import com.skyyeoh.composeweatherforcastapp.utils.formatDate
 import com.skyyeoh.composeweatherforcastapp.utils.formatDecimals
+import com.skyyeoh.composeweatherforcastapp.widgets.HumidityWindPressureRow
+import com.skyyeoh.composeweatherforcastapp.widgets.SunriseSunsetRow
 import com.skyyeoh.composeweatherforcastapp.widgets.WeatherAppBar
+import com.skyyeoh.composeweatherforcastapp.widgets.WeatherDetailRow
+import com.skyyeoh.composeweatherforcastapp.widgets.WeatherStateImage
+import com.skyyeoh.composeweatherforcastapp.widgets.mockWeatherItemList
 
 @Composable
 fun MainScreen(navController: NavController, mainViewModel: MainViewModel = hiltViewModel()) {
@@ -63,7 +73,7 @@ fun MainScaffold(weather: Weather?, navController: NavController) {
             Log.d("TAG", "MainScaffold: Button Clicked")
         }
     }) {
-        MainContent(data = weather, modifier = Modifier.padding(it))
+        MainContent(data = weather, modifier = Modifier.padding(top = it.calculateTopPadding()) )
     }
 }
 
@@ -75,8 +85,9 @@ fun MainContent(data: Weather?, modifier: Modifier = Modifier) {
 
     Column(
         modifier = modifier
-            .fillMaxWidth(),
-        verticalArrangement = Arrangement.Center,
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(text = data?.list?.first()?.dt?.let { formatDate(it) } ?: "Tue, May 5 (EMPTY CASE)",
@@ -98,25 +109,35 @@ fun MainContent(data: Weather?, modifier: Modifier = Modifier) {
             ) {
                 WeatherStateImage(imageUrl = imageUrl)
                 Text(text = data?.list?.first()?.temp?.day?.let { formatDecimals(it) + "°" }
-                    ?: ("[EMPTY CASE] 56 " + "°"), style = MaterialTheme.typography.headlineMedium,
+                    ?: ("[EC] 56 " + "°"), style = MaterialTheme.typography.headlineMedium,
                     fontWeight = FontWeight.ExtraBold)
                 Text(
-                    text = data?.list?.first()?.weather?.first()?.main ?: "Snow [EMPTY CASE]",
+                    text = data?.list?.first()?.weather?.first()?.main ?: "Snow [EC]",
                     fontStyle = FontStyle.Italic
                 )
             }
         }
+        HumidityWindPressureRow(weather = data?.list?.first())
+        HorizontalDivider(Modifier, DividerDefaults.Thickness, DividerDefaults.color)
+        SunriseSunsetRow(weather = data?.list?.first())
 
-
+        Text(
+            text = "This Week", style = MaterialTheme.typography.bodyLarge,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 16.dp, bottom = 6.dp)
+        )
+        Surface(modifier = Modifier
+            .fillMaxWidth()
+            .weight(1f),
+            color = Color(0xFFEEF1EF),
+            shape = RoundedCornerShape(size = 14.dp)
+        ) {
+            LazyColumn(modifier = Modifier.padding(2.dp),
+                contentPadding = PaddingValues(2.dp)) {
+                items(items = data?.list ?: mockWeatherItemList()) { item: WeatherItem ->
+                    WeatherDetailRow(weather = item)
+                }
+            }
+        }
     }
-}
-
-@Composable
-fun WeatherStateImage(imageUrl: String) {
-    AsyncImage(
-        model = imageUrl, contentDescription = "Weather State Image",
-        modifier = Modifier.size(80.dp),
-        placeholder = painterResource(id = android.R.drawable.ic_menu_gallery),
-        error = painterResource(id = android.R.drawable.ic_dialog_alert)
-    )
 }
---
+++ app/src/main/java/com/skyyeoh/composeweatherforcastapp/widgets/Widgets.kt
@@ -0,0 +1,287 @@
+package com.skyyeoh.composeweatherforcastapp.widgets
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight.Companion.SemiBold
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import com.bawp.jetweatherforecast.model.WeatherItem
+import com.bawp.jetweatherforecast.model.WeatherObject
+import com.skyyeoh.composeweatherforcastapp.R
+import com.skyyeoh.composeweatherforcastapp.utils.formatDate
+import com.skyyeoh.composeweatherforcastapp.utils.formatDateTime
+import com.skyyeoh.composeweatherforcastapp.utils.formatDecimals
+
+@Composable
+fun SunriseSunsetRow(weather: WeatherItem?) {
+    Row(
+        modifier = Modifier
+            .padding(top = 16.dp, bottom = 6.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+
+        Row(modifier = Modifier.padding(4.dp)) {
+            Icon(
+                painter = painterResource(id = R.drawable.sunrise),
+                contentDescription = "Sunrise icon",
+                modifier = Modifier.size(30.dp)
+            )
+            Text(text = weather?.sunrise?.let { formatDateTime(it) } ?: "6:00 AM",
+                style = MaterialTheme.typography.labelSmall)
+        }
+
+        Row(modifier = Modifier.padding(4.dp)) {
+            Text(text = weather?.sunset?.let { formatDateTime(it) } ?: "8:00 PM",
+                style = MaterialTheme.typography.labelSmall)
+            Icon(
+                painter = painterResource(id = R.drawable.sunset),
+                contentDescription = "Sunset icon",
+                modifier = Modifier.size(30.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun HumidityWindPressureRow(weather: WeatherItem?) {
+
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+
+        Row(modifier = Modifier.padding(4.dp)) {
+            Icon(
+                painter = painterResource(id = R.drawable.humidity),
+                contentDescription = "humidity icon",
+                modifier = Modifier.size(20.dp)
+            )
+            Text(
+                text = "${weather?.humidity}%",
+                style = MaterialTheme.typography.labelSmall
+            )
+        }
+
+        Row(modifier = Modifier.padding(4.dp)) {
+            Icon(
+                painter = painterResource(id = R.drawable.pressure),
+                contentDescription = "Pressure icon",
+                modifier = Modifier.size(20.dp)
+            )
+            Text(
+                text = "${weather?.pressure} psi",
+                style = MaterialTheme.typography.labelSmall
+            )
+        }
+
+        Row(modifier = Modifier.padding(4.dp)) {
+            Icon(
+                painter = painterResource(id = R.drawable.wind),
+                contentDescription = "Wind icon",
+                modifier = Modifier.size(20.dp)
+            )
+            Text(
+                text = "${weather?.humidity} mph",
+                style = MaterialTheme.typography.labelSmall
+            )
+        }
+
+    }
+
+}
+
+@Composable
+fun WeatherStateImage(imageUrl: String) {
+    AsyncImage(
+        model = imageUrl, contentDescription = "Weather State Image",
+        modifier = Modifier.size(80.dp),
+        placeholder = painterResource(id = android.R.drawable.ic_menu_gallery),
+        error = painterResource(id = android.R.drawable.ic_dialog_alert)
+    )
+}
+
+
+@Composable
+fun WeatherDetailRow(weather: WeatherItem) {
+    val imageUrl = "https://openweathermap.org/img/wn/10d.png" // the 10d is where the icon is
+//    val imageUrl = "https://openweathermap.org/img/wn/${data?.list.first().weather.first().icon}.png"
+
+    Surface(modifier = Modifier.padding(4.dp).fillMaxWidth(),
+        shape = CircleShape.copy(topEnd = CornerSize(6.dp)),
+        color = Color.White) {
+        Row(modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween) {
+            Text(text = formatDate(weather.dt)
+                .split(",").first(), modifier = Modifier.padding(start = 6.dp))
+            WeatherStateImage(imageUrl = imageUrl)
+            Surface(
+                modifier = Modifier
+                    .padding(4.dp),
+                shape = CircleShape,
+                color = Color(0xFFFFC400)
+            ) {
+                Text(text = weather.weather.first().description,
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(4.dp))
+            }
+
+            Text(text = buildAnnotatedString {
+                withStyle(style = SpanStyle(
+                    color = Color.Blue.copy(alpha = 0.7f),
+                    fontWeight = SemiBold
+                )) {
+                    append(formatDecimals(weather.temp.max) + "°")
+                }
+                withStyle(style = SpanStyle(
+                    color = Color.LightGray
+                )) {
+                    append(formatDecimals(weather.temp.min) + "°")
+                }
+            })
+        }
+
+    }
+}
+
+fun mockWeatherItemList(): List<WeatherItem> {
+    return listOf(
+        WeatherItem(
+            clouds = 90,
+            deg = 180,
+            dt = 123456789,
+            feels_like = com.bawp.jetweatherforecast.model.FeelsLike(day = 55.0, night = 54.0, eve = 56.0, morn = 51.0),
+            gust = 0.0,
+            humidity = 80,
+            temp = com.bawp.jetweatherforecast.model.Temp(day = 56.0, min = 50.0, max = 60.0, night = 55.0, eve = 57.0, morn = 52.0),
+            pressure = 1013,
+            weather = listOf(
+                WeatherObject(
+                    description = "light rain",
+                    icon = "10d",
+                    id = 500,
+                    main = "Rain"
+                )
+            ),
+            speed = 5.0,
+            sunrise = 123456789,
+            sunset = 123456789,
+            pop = 0.2,
+            rain = 0.0,
+        ),
+        WeatherItem(
+            clouds = 90,
+            deg = 180,
+            dt = 123456789,
+            feels_like = com.bawp.jetweatherforecast.model.FeelsLike(day = 55.0, night = 54.0, eve = 56.0, morn = 51.0),
+            gust = 0.0,
+            humidity = 80,
+            temp = com.bawp.jetweatherforecast.model.Temp(day = 56.0, min = 50.0, max = 60.0, night = 55.0, eve = 57.0, morn = 52.0),
+            pressure = 1013,
+            weather = listOf(
+                WeatherObject(
+                    description = "light rain",
+                    icon = "10d",
+                    id = 500,
+                    main = "Rain"
+                )
+            ),
+            speed = 5.0,
+            sunrise = 123456789,
+            sunset = 123456789,
+            pop = 0.2,
+            rain = 0.0,
+        ),
+        WeatherItem(
+            clouds = 90,
+            deg = 180,
+            dt = 123456789,
+            feels_like = com.bawp.jetweatherforecast.model.FeelsLike(day = 55.0, night = 54.0, eve = 56.0, morn = 51.0),
+            gust = 0.0,
+            humidity = 80,
+            temp = com.bawp.jetweatherforecast.model.Temp(day = 56.0, min = 50.0, max = 60.0, night = 55.0, eve = 57.0, morn = 52.0),
+            pressure = 1013,
+            weather = listOf(
+                WeatherObject(
+                    description = "light rain",
+                    icon = "10d",
+                    id = 500,
+                    main = "Rain"
+                )
+            ),
+            speed = 5.0,
+            sunrise = 123456789,
+            sunset = 123456789,
+            pop = 0.2,
+            rain = 0.0,
+        ),
+        WeatherItem(
+            clouds = 90,
+            deg = 180,
+            dt = 123456789,
+            feels_like = com.bawp.jetweatherforecast.model.FeelsLike(day = 55.0, night = 54.0, eve = 56.0, morn = 51.0),
+            gust = 0.0,
+            humidity = 80,
+            temp = com.bawp.jetweatherforecast.model.Temp(day = 56.0, min = 50.0, max = 60.0, night = 55.0, eve = 57.0, morn = 52.0),
+            pressure = 1013,
+            weather = listOf(
+                WeatherObject(
+                    description = "light rain",
+                    icon = "10d",
+                    id = 500,
+                    main = "Rain"
+                )
+            ),
+            speed = 5.0,
+            sunrise = 123456789,
+            sunset = 123456789,
+            pop = 0.2,
+            rain = 0.0,
+        ),
+        WeatherItem(
+            clouds = 90,
+            deg = 180,
+            dt = 123456789,
+            feels_like = com.bawp.jetweatherforecast.model.FeelsLike(day = 55.0, night = 54.0, eve = 56.0, morn = 51.0),
+            gust = 0.0,
+            humidity = 80,
+            temp = com.bawp.jetweatherforecast.model.Temp(day = 56.0, min = 50.0, max = 60.0, night = 55.0, eve = 57.0, morn = 52.0),
+            pressure = 1013,
+            weather = listOf(
+                WeatherObject(
+                    description = "light rain",
+                    icon = "10d",
+                    id = 500,
+                    main = "Rain"
+                )
+            ),
+            speed = 5.0,
+            sunrise = 123456789,
+            sunset = 123456789,
+            pop = 0.2,
+            rain = 0.0,
+        )
+    )
+}
